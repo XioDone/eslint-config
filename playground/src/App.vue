63c16a8f0f2a14a90b3c4740b9ca3e2752e94da4
Doxo <gentleDone@outlook.com>
2024-07-11 07:41:56 +0800

    refactor: new playground

---
+++ playground/src/App.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="dark">
+    <header pt-10 fs-center>
+      <h1 from="#d566c2" to="#009ff7" inline-block bg-gradient-to-br bg-clip-text fs-8xl c-transparent font-extrabold>
+        Unoist
+      </h1>
+      <p pt-5 fs-xl>
+        Make using UnoCSS simpler and easier.
+      </p>
+    </header>
+    <main py-10>
+      <div feed-box>
+        <ContentDoc v-for="(doc, i) in docs" :key="i" :doc />
+      </div>
+    </main>
+  </div>
+</template>
+
+<style>
+::-webkit-scrollbar {
+  width: 15px;
+  display: block;
+  background: transparent;
+}
+::-webkit-scrollbar-track {
+  background: transparent;
+}
+::-webkit-scrollbar-thumb {
+  min-height: 100px;
+  border: 6px solid transparent;
+  display: block;
+  background: #787878;
+  border-radius: 10px;
+  background-clip: padding-box;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+body {
+  text-rendering: optimizeLegibility;
+  -webkit-font-smoothing: antialiased;
+  --at-apply: 'bg-cool-gray-950 c-light-9 font-nunito';
+}
+</style>
---
+++ playground/src/components/ContentDoc.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+defineProps<{ doc: Record<string, any> }>()
+</script>
+
+<template>
+  <div mb-10>
+    <div flex items-end gap-6>
+      <h3 mb-2 fs-4xl c-gray-3 font-600>
+        {{ doc.title }}
+        <span v-if="doc.alias" fs-2xl>
+          <span ml-5px mr-10px c-gray-6>alias:</span>
+          <span c-primary>{{ doc.alias }}</span>
+        </span>
+      </h3>
+      <div py-2 fs-xl c-gray-4>
+        {{ doc.desc }}
+      </div>
+    </div>
+    <div grid="~ cols-2 gap-5" p-5 fs-18px card>
+      <code>
+        <p mb-2 c-primary>
+          {{ doc.code }}
+        </p>
+        <div max-w-380px rd bg-dark-200 p-10px>
+          <p v-for="item in doc.compiled" :key="item">{{ item }}</p>
+        </div>
+      </code>
+
+      <div>
+        <p pb-2>
+          Preview
+        </p>
+        <div :class="doc.preview" b="~ dark-1 dashed">
+          {{ doc.code }}
+        </div>
+        <span v-if="doc.preview === 'hide'" fs-12px>You can't see me :)</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
